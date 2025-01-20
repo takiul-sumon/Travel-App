151236c4132e61a43d254ac29c93b9a8f1b523d4
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-21 02:47:14 +0600

    Font Vacation

--- lib/Splash_Screan.dart
+++ lib/Splash_Screan.dart
@@ -13,16 +13,50 @@ class Splash_Screan extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
+        child: const Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 20,
+            ),
             Align(
+              // widthFactor: .5,
               alignment: Alignment.center,
               child: Text(
                 'Aspen',
                 style: TextStyle(
-                    fontSize: 50, color: Colors.white, fontFamily: 'Hiatus'),
+                    fontSize: 116, color: Colors.white, fontFamily: 'Hiatus'),
               ),
             ),
+            SizedBox(
+              height: 300,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  'Luxarious\nVacation',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontFamily: 'Montserrat-SemiBold',
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+
+            // ),
+            // Positioned(child: Text(
+            //     'Aspen',
+            //     style: TextStyle(
+            //         fontSize: 116, color: Colors.white, fontFamily: 'Hiatus'),
+            //   ),
+            //   left: 260,top: 126,)
+
+            // Padding(padding: EdgeInsets.fromLTRB(263, 126, 0, 0)),
           ],
         ),
       ),

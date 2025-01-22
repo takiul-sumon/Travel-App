88e8b48ea6e32c4a342a804913ff00a5fb2b24ff
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-22 22:59:05 +0600

    Tap Bar

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,80 +5,126 @@ class ChooseHotel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    List<String> hotelChoice = [
+      'Locations',
+      'Hotels',
+      'Food',
+      'Adventure',
+      'Actions'
+    ];
+
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        backgroundColor: Colors.white,
         body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 44),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 44),
+          child: Column(
             children: [
-              const Column(
+              // Header Section
+              Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text(
-                    'Explore',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Montserrat'),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Explore',
+                        style:
+                            TextStyle(fontSize: 14, fontFamily: 'Montserrat'),
+                      ),
+                      Text(
+                        'Aspen',
+                        style: TextStyle(
+                            fontSize: 32,
+                            fontFamily: 'Montserrat-Regular',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'Aspen',
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontFamily: 'Montserrat-Regular',
-                        fontWeight: FontWeight.bold),
+                  Row(
+                    children: [
+                      Container(
+                        height: 16,
+                        width: 16,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icons/icon1.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      const Text('Aspen, Usa',
+                          style: TextStyle(
+                              fontSize: 12, fontFamily: 'circularxx')),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Container(
+                        height: 16,
+                        width: 16,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icons/icon2.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Row(
-                children: [
-                  Container(
-                    height: 16,
-                    width: 16,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/icons/icon1.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+              const SizedBox(height: 20),
+              // Search Bar
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Find things to do',
+                  fillColor: const Color(0xffA8CCF0).withOpacity(.2),
+                  filled: true,
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
                   ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  const Text('Aspen, Usa',
-                      style: TextStyle(fontSize: 12, fontFamily: 'circularxx')),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  Container(
-                    height: 16,
-                    width: 16,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/icons/icon2.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // TabBar Section
+              TabBar(
+                labelColor: Colors.blue,
+                unselectedLabelColor: Colors.grey,
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: Color(0xff176FF2).withOpacity(.2),
+                ),
+                // indicatorColor: Colors.blue,
+                 dividerColor: Colors.transparent,
+                tabs: const [
+                  Text("Locations",style: TextStyle(),),
+                  Tab(text: "Hotels"),
+                  Tab(text: "Food"),
+                  Tab(text: "Adventure"),
+
+
                 ],
-              )
+              ),
+            const  Expanded(
+                // TabBarView for tab content
+                child: TabBarView(
+                  children: [
+                    Center(child: Text("Content for Locations")),
+                    Center(child: Text("Content for Hotels")),
+                    Center(child: Text("Content for Food")),
+                  ],
+                ),
+              ),
             ],
           ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Find things to do',
-              fillColor: Color(0xffA8CCF0).withOpacity(.2),
-              filled: true,
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/Splash_Screan.dart';
+import 'package:travel_app/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Splash_Screan(),
+      home: ChooseHotel(),
     );
   }
 }

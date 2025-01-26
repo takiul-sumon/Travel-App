b7730b36cb4995c82b5e46261570aa85b120d8df
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-27 01:19:26 +0600

    Scroll Bar

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ChooseHotel extends StatelessWidget {
+class ChooseHotel extends StatefulWidget {
   const ChooseHotel({super.key});
 
+  @override
+  State<ChooseHotel> createState() => _ChooseHotelState();
+}
+
+class _ChooseHotelState extends State<ChooseHotel> {
   @override
   Widget build(BuildContext context) {
     List<String> hotelChoice = [
@@ -13,116 +18,130 @@ class ChooseHotel extends StatelessWidget {
       'Actions'
     ];
 
-    return DefaultTabController(
-      length: 3, // Number of tabs
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 44),
-          child: Column(
-            children: [
-              // Header Section
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Explore',
-                        style:
-                            TextStyle(fontSize: 14, fontFamily: 'Montserrat'),
-                      ),
-                      Text(
-                        'Aspen',
-                        style: TextStyle(
-                            fontSize: 32,
-                            fontFamily: 'Montserrat-Regular',
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        height: 16,
-                        width: 16,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/icons/icon1.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 6,
-                      ),
-                      const Text('Aspen, Usa',
-                          style: TextStyle(
-                              fontSize: 12, fontFamily: 'circularxx')),
-                      const SizedBox(
-                        width: 6,
-                      ),
-                      Container(
-                        height: 16,
-                        width: 16,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/icons/icon2.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              // Search Bar
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Find things to do',
-                  fillColor: const Color(0xffA8CCF0).withOpacity(.2),
-                  filled: true,
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              // TabBar Section
-              TabBar(
-                labelColor: Colors.blue,
-                unselectedLabelColor: Colors.grey,
-                indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24),
-                  color: Color(0xff176FF2).withOpacity(.2),
-                ),
-                // indicatorColor: Colors.blue,
-                 dividerColor: Colors.transparent,
-                tabs: const [
-                  Text("Locations",style: TextStyle(),),
-                  Tab(text: "Hotels"),
-                  Tab(text: "Food"),
-                  Tab(text: "Adventure"),
+    final List<String> tabs = [
+      'Location',
+      'Hotels',
+      'Food',
+      'Adventure',
+      'Activities'
+    ];
+    String selectedTab = 'Location';
 
-
-                ],
-              ),
-            const  Expanded(
-                // TabBarView for tab content
-                child: TabBarView(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 44),
+        child: Column(
+          children: [
+            // Header Section
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Center(child: Text("Content for Locations")),
-                    Center(child: Text("Content for Hotels")),
-                    Center(child: Text("Content for Food")),
+                    Text(
+                      'Explore',
+                      style: TextStyle(fontSize: 14, fontFamily: 'Montserrat'),
+                    ),
+                    Text(
+                      'Aspen',
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontFamily: 'Montserrat-Regular',
+                          fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
+                Row(
+                  children: [
+                    Container(
+                      height: 16,
+                      width: 16,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/icon1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    const Text('Aspen, Usa',
+                        style:
+                            TextStyle(fontSize: 12, fontFamily: 'circularxx')),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Container(
+                      height: 16,
+                      width: 16,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/icon2.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            // Search Bar
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Find things to do',
+                fillColor: const Color(0xffA8CCF0).withOpacity(.2),
+                filled: true,
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: tabs.map(
+                    (e) {
+                      final bool isSelected = selectedTab == e;
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                            // margin: const EdgeInsets.symmetric(horizontal: 8),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: isSelected
+                                  ? Colors.blue.withOpacity(0.1)
+                                  : Colors.transparent, // Background for selected
+                              borderRadius:
+                                  BorderRadius.circular(20), // Rounded corners
+                            ),
+                            child: Text(
+                              e,
+                              style: TextStyle(
+                                color: isSelected
+                                    ? Colors.blue
+                                    : Colors
+                                        .grey, // Text color based on selection
+                                fontWeight: isSelected
+                                    ? FontWeight.bold
+                                    : FontWeight.normal, // Bold for selected
+                                fontSize: 16,
+                              ),
+                            )),
+                      );
+                    },
+                  ).toList()),
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/Splash_Screan.dart';
 import 'package:travel_app/dashboard.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       title: 'Flutter Demo',
       home: ChooseHotel(),
     );
